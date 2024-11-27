99265bfc58a88e75daaf9b4d9eadd74ad2fe8a7a
Your Name <you@example.com>
2024-11-27 12:18:50 +0300

    HW10 compile domain regexp once

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -23,21 +23,25 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	stats, err := getUsers(r, domain)
+	domainRegexp, err := regexp.Compile("\\." + domain)
+	if err != nil {
+		return nil, err
+	}
+	stats, err := getUsers(r, domainRegexp)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
 	return stats, nil
 }
 
-func getUsers(r io.Reader, domain string) (DomainStat, error) {
+func getUsers(r io.Reader, domainRegexp *regexp.Regexp) (DomainStat, error) {
 	scanner := bufio.NewScanner(r)
 	stats := make(DomainStat)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		err := countUserDomain(fastjson.GetString([]byte(line), "Email"), domain, stats)
+		err := countUserDomain(fastjson.GetString([]byte(line), "Email"), domainRegexp, stats)
 		if err != nil {
 			return nil, err
 		}
@@ -45,13 +49,8 @@ func getUsers(r io.Reader, domain string) (DomainStat, error) {
 	return stats, nil
 }
 
-func countUserDomain(email string, domain string, stat DomainStat) error {
-
-	matched, err := regexp.Match("\\."+domain, []byte(email))
-	if err != nil {
-		return nil
-	}
-
+func countUserDomain(email string, domainRegexp *regexp.Regexp, stat DomainStat) error {
+	matched := domainRegexp.Match([]byte(email))
 	if matched {
 		emailDomain := strings.ToLower(strings.SplitN(email, "@", 2)[1])
 		stat[emailDomain]++
