a7a9fa82047c779ce7859061c53180a7d649ff71
Your Name <you@example.com>
2024-11-27 14:59:18 +0300

    HW11 initial telnet client implementation

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,59 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
 func main() {
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	port := os.Args[len(os.Args)-1]
+	host := os.Args[len(os.Args)-2]
+
+	addr := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(addr, *timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	go func() {
+		for {
+			err := client.Send()
+			if errors.Is(err, io.EOF) {
+				println("...EOF")
+				client.Close()
+				os.Exit(0)
+			}
+		}
+	}()
+	go func() {
+		for {
+			err := client.Receive()
+			if errors.Is(err, io.EOF) {
+				println("...Connection was closed by peer")
+				client.Close()
+				os.Exit(0)
+			}
+		}
+	}()
+
+	<-ctx.Done()
+	client.Close()
+	stop()
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"io"
+	"net"
+	"sync"
 	"time"
 )
 
@@ -11,10 +15,82 @@ type TelnetClient interface {
 	Send() error
 	Receive() error
 }
+type Client struct {
+	sync.Mutex
+	addr    string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+
+	return &Client{
+		addr:    address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (tc *Client) Connect() error {
+	tc.Lock()
+	defer tc.Unlock()
+	d := net.Dialer{Timeout: tc.timeout}
+	conn, err := d.Dial("tcp", tc.addr)
+	if err != nil {
+		return err
+	}
+	tc.conn = conn
+
+	println("...Connected to", tc.addr)
 	return nil
+}
+
+func (tc *Client) Send() error {
+	reader := bufio.NewReader(tc.in)
+	readed, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	// log.Printf("send: readed %d bytes", len(readed))
+	writen, err := tc.conn.Write([]byte(readed))
+	if err != nil {
+		return err
+	}
+	// log.Printf("send: written %d bytes", writen)
+	if len(readed) != writen {
+		return errors.New("failed to send whole message")
+	}
+	return nil
+}
+
+func (tc *Client) Receive() error {
+	buf := make([]byte, 32768)
+	readed, err := tc.conn.Read(buf)
+	if err != nil {
+		return err
+	}
+	// log.Printf("receive: read %d bytes", readed)
+	buf = buf[:readed]
+	written, err := tc.out.Write(buf)
+	if err != nil {
+		return err
+	}
+	// log.Printf("receive: written %d bytes", written)
+	if readed != written {
+		return errors.New("failed to read whole message")
+	}
+	return nil
+}
+
+func (tc *Client) Close() error {
+	tc.Lock()
+	defer tc.Unlock()
+
+	return tc.conn.Close()
+
 }
 
 // Place your code here.
